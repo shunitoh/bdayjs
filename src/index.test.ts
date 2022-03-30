703b414097a7686b7f84351998163c1f02625146
shunitoh <shun.itoh@gmail.com>
2022-03-30 15:35:40 +0900

    modify

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,15 @@
 import test from "ava";
-import{ addHoliday, add, subtract } from "./index";
+import dayjs from "dayjs";
+import BusinessDay from "./";
+import "dayjs/locale/ja";
+dayjs.locale("ja");
+const businessDay = new BusinessDay({dj: dayjs, country: 'JP'})
 
 test("60時間遡る 休日が3日ある 対象日が休日 hour単位", (t) => {
   // 1/10(成人式) => +24, 1/9(日) => +24, 1/8(土) => +24, 1/7, 1/6, 1/5
-  const hour = addHoliday("2022-01-10 03:00:00", "subtract", 60, "hours");
-  const subtractDate = subtract("2022-01-10 03:00:00", 60, "hours");
-  const addDate = add("2022-01-10 03:00:00", 60, "hours");
+  const hour = businessDay.addHoliday({day: "2022-01-10 03:00:00", mode: "subtract", num: 60, unit: "hours"});
+  const subtractDate = businessDay.subtract("2022-01-10 03:00:00", 60, "hours");
+  const addDate = businessDay.add("2022-01-10 03:00:00", 60, "hours");
   const expectHour = 24 * 3 + 60;
   const expectSubtractDate = "2022-01-04 15:00:00";
   const expectAddDate = "2022-01-13 15:00:00";
@@ -16,8 +20,8 @@ test("60時間遡る 休日が3日ある 対象日が休日 hour単位", (t) => 
 
 test("60時間遡る 休日が3日ある 対象日が営業日 hour単位", (t) => {
   // 1/10(成人式) => +24, 1/9(日) => +24, 1/8(土) => +24, 1/7, 1/6, 1/5
-  const hour = addHoliday("2022-01-11 10:00:00", "subtract", 60, "hours");
-  const date = subtract("2022-01-11 10:00:00", 60, "hours");
+  const hour = businessDay.addHoliday({day: "2022-01-11 10:00:00", mode: "subtract", num: 60, unit: "hours"});
+  const date = businessDay.subtract("2022-01-11 10:00:00", 60, "hours");
   const expectHour = 24 * 3 + 60;
   const expectDate = "2022-01-05 22:00:00";
   t.is(hour, expectHour);
@@ -26,8 +30,8 @@ test("60時間遡る 休日が3日ある 対象日が営業日 hour単位", (t) 
 
 test("60時間遡る 休日が3日ある 対象日が休日 day単位", (t) => {
   // 1/10(成人式) => +24, 1/9(日) => +24, 1/8(土) => +24, 1/7, 1/6, 1/5
-  const hour = addHoliday("2022-01-10 03:00:00", "subtract", 3, "days");
-  const date = subtract("2022-01-10 03:00:00", 3, "days");
+  const hour = businessDay.addHoliday({day: "2022-01-10 03:00:00", mode: "subtract", num: 3, unit: "days"});
+  const date = businessDay.subtract("2022-01-10 03:00:00", 3, "days");
   const expectDays = 3 + 3;
   const expectDate = "2022-01-04 03:00:00";
   t.is(hour, expectDays);
@@ -36,8 +40,8 @@ test("60時間遡る 休日が3日ある 対象日が休日 day単位", (t) => {
 
 test("60時間遡る 休日が3日ある 対象日が営業日 day単位", (t) => {
   // 1/10(成人式) => +24, 1/9(日) => +24, 1/8(土) => +24, 1/7, 1/6, 1/5
-  const hour = addHoliday("2022-01-11 10:00:00", "subtract", 3, "days");
-  const date = subtract("2022-01-11 10:00:00", 3, "days");
+  const hour = businessDay.addHoliday({day: "2022-01-11 10:00:00", mode: "subtract", num: 3, unit: "days"});
+  const date = businessDay.subtract("2022-01-11 10:00:00", 3, "days");
   const expectHour = 3 + 3;
   const expectDate = "2022-01-05 10:00:00";
   t.is(hour, expectHour);
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,47 @@
-import * as lodash from "lodash";
-import Holidays from "date-holidays";
+import Holidays, {HolidaysTypes} from "date-holidays";
 import dayjs from "dayjs";
-import "dayjs/locale/ja";
-dayjs.locale("ja");
-const hd = new Holidays("JP");
+import {get as lGet } from 'lodash';
 
-export const addHoliday = (
-  day,
-  mode: "subtract" | "add",
-  num,
-  unit: "hours" | "days" = "hours"
-) => {
-  let targetDay = dayjs(day);
-  let days = unit === "hours" ? Math.ceil(num / 24) : num;
-  let holidayCount = 0;
-  while (days > 0) {
-    // 祝日か土日だったらカウントアップする
-    if (hd.isHoliday(targetDay.toDate()) || [0, 6].includes(targetDay.day())) {
-      holidayCount++;
-      days++;
-    }
-    targetDay = targetDay[mode](1, "days");
-    days--;
+type AddHolidayType = {
+  day?: string;
+  mode: "subtract" | "add";
+  num: number;
+  unit: "hours" | "days";
+}
+
+type BusinessDayType = {
+  dj?;
+  country?: HolidaysTypes.Country | string ;
+}
+
+export default class BusinessDay {
+  private readonly hd;
+  private readonly dj;
+  constructor(request: BusinessDayType) {
+    this.hd = new Holidays(lGet(request, 'country', 'JP'));
+    this.dj = lGet(request, 'dayjs', dayjs);
   }
-  return unit === "hours" ? num + holidayCount * 24 : num + holidayCount;
-};
+  addHoliday(request: AddHolidayType) {
+    const {mode, num, unit} = request;
+    const day = lGet(request, 'day');
 
-export const subtract = (day, num, unit: "hours" | "days" = "hours") =>
-  dayjs(day).subtract(addHoliday(day, "subtract", num, unit), unit);
-export const add = (day, num, unit: "hours" | "days" = "hours") =>
-  dayjs(day).add(addHoliday(day, "add", num, unit), unit);
+    let targetDay = day ? this.dj(day) : this.dj();
+    let days = unit === "hours" ? Math.ceil(num / 24) : num;
+    let holidayCount = 0;
+    while (days > 0) {
+      // Count up if it's a holiday or weekend
+      if (this.hd.isHoliday(targetDay.toDate()) || [0, 6].includes(targetDay.day())) {
+        holidayCount++;
+        days++;
+      }
+      targetDay = targetDay[mode](1, "days");
+      days--;
+    }
+    return unit === "hours" ? num + holidayCount * 24 : num + holidayCount;
+  }
+  public subtract = (day, num, unit: "hours" | "days" = "hours") =>
+      this.dj(day).subtract(this.addHoliday({day, mode: "subtract", num, unit}), unit);
+  public add = (day, num, unit: "hours" | "days" = "hours") =>
+      this.dj(day).add(this.addHoliday({day, mode: "add", num, unit}), unit);
+}
+
